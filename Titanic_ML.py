6876a69e0b030754b25bb83ca3f477c07a2ca678
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-04-25 15:43:51 +0530

    Inital Commit

---
+++ Titanic_ML.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 25 15:37:26 2021
+
+@author: bjish
+"""
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sb
+
